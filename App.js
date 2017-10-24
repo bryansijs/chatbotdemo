c0a32fab304e4854fe4fb8ee3b2832c8474d6361
jeroenbertels <jeroen.bertels@essent.nl>
2017-10-24 14:59:19 +0200

    Commit for bryan and push for bryan, code is not up to standards yet

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 const agentBot = require('./lib/agentBot');
 
 //TODO: Hardcoded value's to config file or env variables.
-var agent = new agentBot(34252425, "Chatbot", "chatbotessent123!");
+// please be and dont steal this (:
+const agent = new agentBot(63494234, "essentBot", "essent123");
 
 agent.start();
