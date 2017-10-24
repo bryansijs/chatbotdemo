3904ba47157aaf25c6a5798457e967ac6f421aea
jeroenbertels <jeroen.bertels@essent.nl>
2017-10-24 14:59:35 +0200

    nice

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 const agentBot = require('./lib/agentBot');
 
 //TODO: Hardcoded value's to config file or env variables.
-// please be and dont steal this (:
+// please be nice and dont steal this (:
 const agent = new agentBot(63494234, "essentBot", "essent123");
 
 agent.start();
